c2a18b22112243c4c72b7009afc10f0290a47816
MatiasBaezaWong <m-baeza@outlook.cl>
2025-11-15 17:41:49 -0300

    Despliegue en Render (Arreglo settings.py)

--- hireup/settings.py
+++ hireup/settings.py
@@ -31,9 +31,19 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["https://hireup-g53h.onrender.com"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://" + RENDER_EXTERNAL_HOSTNAME if RENDER_EXTERNAL_HOSTNAME else "",
+]    
 
 # Application definition
 
