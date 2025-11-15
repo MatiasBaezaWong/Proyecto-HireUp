9efcdb7be5eed99052c5936cc636e52a44668a1f
MatiasBaezaWong <m-baeza@outlook.cl>
2025-11-15 17:23:50 -0300

    Despliegue en Render (Arreglo settings.py)

--- hireup/settings.py
+++ hireup/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('https://hireup-g53h.onrender.com/')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
